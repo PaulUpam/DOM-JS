befd06fee654a227d18f85c2e9783dfd42431940
PaulUpam <paulupam@gmail.com>
2023-12-18 16:15:04 +0600

    enter button add

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const getClick = document.querySelector("#userClick");
-
-getClick.addEventListener("click", function () {
+ function getClick () {
   //get user input
   const getInput = document.querySelector("#userInput");
 
@@ -19,4 +17,14 @@ getClick.addEventListener("click", function () {
   countrylist.append(listItem);
   // Clear the input field after adding the item
   getInput.value = "";
-});
+};
+function KeyPress(event) {
+  //if the pressed key is Enter
+  if (event.keyCode === 13) {
+      // Prevent the default action of the Enter key (form submission)
+      event.preventDefault();
+
+      // Trigger the "Add country" functionality
+      getClick ();
+  }
+}
